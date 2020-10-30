9c34ae3f8d577320bdc7cf6e40343bd55916ccd4
close-7 <531210222@qq.com>
2020-10-31 02:50:57 +0800

    proxy

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+// webpack的配置表
+module.exports = {
+    devServer:{
+        host:'localhost',
+        port:8080,
+        proxy:{
+            '/api':{
+                target:'https://www.imooc.com',
+                changeOrigin:false
+            }
+        }
+    }
+}
